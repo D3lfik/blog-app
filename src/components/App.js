4b9ec99dc829a2b0ed1aff044c916107d8c60afd
Apurv <apurv.mod007@gmail.com>
2023-09-20 17:51:47 +0530

    deployed firebase

--- src/components/App.js
+++ src/components/App.js
@@ -8,16 +8,15 @@ import CreatePost from './CreatePost';
 function App() {
   return (
     <div className="container">
-        <Navbar/>
+        
         <Router>
+        <Navbar/>
           <Routes>
           <Route  path='/' element={<Home/>}/>
           <Route exact path='/post/:postId' element={<PostDetail/>}/>
           <Route exact path='/create-post' element={<CreatePost/>}/>
           </Routes>
          </Router>
-
-        Starting React blog
       </div>
   
   );
